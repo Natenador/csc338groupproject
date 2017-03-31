7384d6652e21866e5b0debcc95dc8474359a9aea
Medlin <twm123@live.missouristate.edu>
2017-03-31 18:31:05 -0500

    Added signIn function to login_client module

--- server/login_client.py
+++ server/login_client.py
@@ -1,1 +1,10 @@
 #login_client - Client side login functions
+
+#Below has not been tested
+def signIn():
+	name = input('Enter user name: ')
+	password = ('Enter password: ')
+	print('Name: %s\nPassword: %s' % name,password)
+
+
+	
